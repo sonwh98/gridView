5e90a1a9b90950d7d1a4dc77441840472f32ef26
SonnyTo <sto@csicorporatecard.com>
2014-02-21 22:30:32 -0500

    scroll to bottom

--- posGridView/src/main/java/com/datayumyum/pos/GridViewActivity.java
+++ posGridView/src/main/java/com/datayumyum/pos/GridViewActivity.java
@@ -25,17 +25,20 @@ public class GridViewActivity extends Activity {
         GridView gridview = (GridView) findViewById(R.id.gridview);
         gridview.setAdapter(new ImageAdapter(this));
         final TableLayout tableLayout = (TableLayout) findViewById(R.id.tableLayout);
-
+        final ScrollView scrollView = (ScrollView) findViewById(R.id.scrollView1);
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
                 TableRow tr = new TableRow(getApplicationContext());
                 tr.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
-                TextView textView = new TextView(getApplicationContext());
-                textView.setText("foobar");
-                textView.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
-                tr.addView(textView);
+                for (int i = 0; i < 4; i++) {
+                    TextView textView = new TextView(getApplicationContext());
+                    textView.setText("foobar" + i);
+                    textView.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+                    textView.setPadding(5, 5, 5, 5);
+                    tr.addView(textView);
+                }
                 tableLayout.addView(tr, new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
-                Toast.makeText(GridViewActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+                scrollView.fullScroll(View.FOCUS_DOWN);
             }
         });
     }
