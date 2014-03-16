dd879055668eeb9d10911d1222939de1df32e81b
Sonny To <son.c.to@gmail.com>
2014-03-16 14:01:19 -0400

    rm unused import

--- posGridView/src/main/java/com/datayumyum/pos/CategoryAdapter.java
+++ posGridView/src/main/java/com/datayumyum/pos/CategoryAdapter.java
@@ -7,7 +7,6 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
-import org.json.JSONException;
 
 import java.util.ArrayList;
 import java.util.List;
